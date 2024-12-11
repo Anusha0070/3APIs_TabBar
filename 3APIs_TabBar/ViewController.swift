973bf0ed79047ed3a715d0dc5024d2eaeac93f83
Anusha Raju <anusha.raju0070@gmail.com>
2024-12-11 07:11:46 -0800

    Initial Commit

---
+++ 3APIs_TabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3APIs_TabBar
+//
+//  Created by Anusha Raju on 12/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 3APIs_TabBarTests/_APIs_TabBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  _APIs_TabBarTests.swift
+//  3APIs_TabBarTests
+//
+//  Created by Anusha Raju on 12/11/24.
+//
+
+import Testing
+@testable import _APIs_TabBar
+
+struct _APIs_TabBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
